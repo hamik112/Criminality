85b8162989200a67e7157f66a6c22eac64f9cc7c
Jonathan Schuett <Bioshacker001@gmail.com>
2014-06-20 21:01:36 -0400

    Updated Gameloader with Config warning

--- gamebase/game.c
+++ gamebase/game.c
@@ -4,11 +4,28 @@
 
 gameload()
 {
+/*==============================================*/
+/*      Criminality Initial Gameloader          */
+/*  WARNING: Configuration files should NEVER,  */
+/*  under ANY CIRCUMSTANCES, be stored outside  */
+/*  of ".". EVER!!!!! No official support will  */
+/*  be given to modified gameloaders that have  */
+/*  this feature, as configuration is supposed  */
+/*  to be drop-in, and using system or external */
+/*  directories for this can break this feature.*/
+/*          YOU HAVE BEEN WARNED!!!!!!          */
+/*                                              */
+/* Gameload should use the OFFLINK core builtin */
+/* configuration loader, but custom confloaders */
+/*   are fine as long as they meet the OFFLINK  */
+/* modularity standards. Hey, you're the dev.   */
+/*     I just designed the core engine.         */
+/*==============================================*/
 printf("game1");
 gameconf();
 printf("game2");
 modload();
-
+printf("game3");
 
 
 }
